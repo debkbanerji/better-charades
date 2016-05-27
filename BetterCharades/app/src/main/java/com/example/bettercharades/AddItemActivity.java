507cc9c2eaa759657edd8ec3d33c424a9aee20b1
Deb Banerji <debkbanerji@gmail.com>
2016-05-27 22:30:45 +0530

    Choose category updates

--- BetterCharades/app/src/main/java/com/example/bettercharades/AddItemActivity.java
+++ BetterCharades/app/src/main/java/com/example/bettercharades/AddItemActivity.java
@@ -8,6 +8,7 @@ import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -38,13 +39,57 @@ public class AddItemActivity extends AppCompatActivity {
         try {
 //            fos = openFileOutput(title, Context.MODE_PRIVATE);
             fos = openFileOutput(title + ".category.txt", Context.MODE_PRIVATE);
-            fos.write("TEST".getBytes());
-            fos.close();
+//            fos.write("TEST".getBytes());
+//            fos.close();
         } catch (Exception e) {
             Log.e("File IO Error", e.getMessage());
         }
-        Log.i("IO worked", "No errors writing to/creating file");
+//        Log.i("IO worked", "No errors writing to/creating file");
+
+        itemText = (EditText) findViewById(R.id.itemText);
+
+        addButton = (Button) findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = itemText.toString();
+                if (item.length() > 0) {
+                    try {
+                        fos.write(item.getBytes());
+                    } catch (Exception e) {
+                        Log.e("File IO Error", e.getMessage());
+                    }
+                }
+                itemText.setText("");
+            }
+        });
+
+        finishButton = (Button) findViewById(R.id.finishCreationButton);
+
+        finishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String item = itemText.toString();
+                if (item.length() > 0) {
+                    try {
+                        fos.write(item.getBytes());
+                    } catch (Exception e) {
+                        Log.e("File IO Error", e.getMessage());
+                    }
+                }
+                try {
+                    fos.close();
+                } catch (Exception e) {
+                    Log.e("File IO Error", e.getMessage());
+                }
+                Intent intent = new Intent(AddItemActivity.this, ChooseActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
+            }
+        });
     }
+
 }
 
 
--- BetterCharades/app/src/main/java/com/example/bettercharades/PlayActivity.java
+++ BetterCharades/app/src/main/java/com/example/bettercharades/PlayActivity.java
@@ -1,12 +1,15 @@
 package com.example.bettercharades;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity implements SensorEventListener {
@@ -21,10 +24,23 @@ public class PlayActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+
+
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
         mSensorManager.registerListener(this, mGyroscope, SensorManager.SENSOR_DELAY_GAME);
         infoText = (TextView) findViewById(R.id.orientationText);
+
+        Intent intent = getIntent();
+
+        String title = intent.getStringExtra("CATEGORY");
+        infoText.setText(title);
     }
 
     @Override
@@ -36,7 +52,7 @@ public class PlayActivity extends AppCompatActivity implements SensorEventListen
         } else if (Math.abs(event.values[0]) > 1.0 - tiltThreshold) {
             tiltFactor = 1;
         }
-        infoText.setText(Integer.toString(tiltFactor));
+//        infoText.setText(Integer.toString(tiltFactor));
     }
 
     @Override
